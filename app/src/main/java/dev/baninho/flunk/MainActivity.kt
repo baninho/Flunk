b4e273fcc1642f89745eb2f55d41a5a71ad959a7
baninho <florianimbt@gmail.com>
2021-02-12 17:31:12 -0600

    Add map activity

--- app/src/main/java/dev/baninho/flunk/MainActivity.kt
+++ app/src/main/java/dev/baninho/flunk/MainActivity.kt
@@ -1,11 +1,36 @@
 package dev.baninho.flunk
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.fragment.app.Fragment
+import java.security.AccessController.getContext
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mainFragment: MainFragment
+    private lateinit var activeFragment: Fragment
+    private lateinit var map_button: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mainFragment = MainFragment.newInstance()
+/*
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, mainFragment)
+                    .commitNow()
+            activeFragment = mainFragment
+        }
+ */
+        map_button = findViewById(R.id.map_button)
+        map_button.setOnClickListener {
+            val intent = Intent(this, MapsActivity::class.java)
+            startActivity(intent)
+        }
     }
 }

a906079922117c4bfc50edbe92ea60463e578675
baninho <florianimbt@gmail.com>
2021-02-12 23:35:25 -0600

    Add Courts listener

--- app/src/main/java/dev/baninho/flunk/ui/MainViewModel.kt
+++ app/src/main/java/dev/baninho/flunk/ui/MainViewModel.kt
@@ -1,12 +1,18 @@
 package dev.baninho.flunk.ui
 
+import android.content.ContentValues.TAG
 import android.util.Log
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.FirebaseFirestoreSettings
 import dev.baninho.flunk.dto.Court
 
 class MainViewModel: ViewModel() {
+
+    private var firestore: FirebaseFirestore = FirebaseFirestore.getInstance()
+    private var _courts: MutableLiveData<ArrayList<Court>> = MutableLiveData<ArrayList<Court>>()
+
     fun save(court: Court) {
         firestore.collection("courts")
             .document()
@@ -15,9 +21,38 @@ class MainViewModel: ViewModel() {
             .addOnFailureListener { Log.d("Firebase", "save failed") }
     }
 
-    private var firestore: FirebaseFirestore = FirebaseFirestore.getInstance()
-
     init {
         firestore.firestoreSettings = FirebaseFirestoreSettings.Builder().build()
+        listenToCourts()
     }
+
+    /*
+    Get Updates from Firestore
+     */
+    private fun listenToCourts() {
+        firestore.collection("courts").addSnapshotListener {
+                snapshot, error ->
+            // if there is an exception, skip
+            if (error != null) {
+                Log.w(TAG, "Listen failed", error)
+                return@addSnapshotListener
+            }
+            // if we are here, we did not encounter an exception
+            if (snapshot != null) {
+                val allCourts = ArrayList<Court>()
+                val documents = snapshot.documents
+                documents.forEach {
+                    val court = it.toObject(Court::class.java)
+                    if (court != null) {
+                        allCourts.add(court!!)
+                    }
+                }
+                _courts.value = allCourts
+            } // endif (snapshot!=null)
+        }
+    }
+
+    internal var courts: MutableLiveData<ArrayList<Court>>
+    get() { return _courts }
+    set(value) { courts = value }
 }

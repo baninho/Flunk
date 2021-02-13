1992694848f313aae6d56c4145127146bb234d45
baninho <florianimbt@gmail.com>
2021-02-13 01:16:10 -0600

    Add camera focus (bugged)

--- app/src/main/java/dev/baninho/flunk/ui/MapsActivity.kt
+++ app/src/main/java/dev/baninho/flunk/ui/MapsActivity.kt
@@ -3,18 +3,20 @@ package dev.baninho.flunk.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
+import android.util.Log
 import androidx.lifecycle.Observer
+import com.google.android.gms.maps.*
 
-import com.google.android.gms.maps.CameraUpdateFactory
-import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.OnMapReadyCallback
-import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.LatLngBounds
 import com.google.android.gms.maps.model.MarkerOptions
 import dev.baninho.flunk.R
 import dev.baninho.flunk.dto.Court
+import kotlin.math.pow
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private var padding: Int = 10
 
     private lateinit var courts: List<Court>
     private lateinit var mMap: GoogleMap
@@ -41,21 +43,37 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
         mainViewModel = MainViewModel()
-        mainViewModel.courts.observe(this, Observer {
+        mainViewModel.courts.observe(this, {
             courts -> this.courts = courts
             updateMap()
         })
     }
 
     private fun updateMap() {
-        if (courts != null) {
-            mMap.clear()
-            courts.forEach { court ->
-                if (court.latitude.isNotEmpty() && court.longitude.isNotEmpty()) {
-                    val marker = LatLng(court.latitude.toDouble(), court.longitude.toDouble())
-                    mMap.addMarker(MarkerOptions().position(marker).title(court.toString()))
-                }
+        mMap.clear()
+        val markers: ArrayList<MarkerOptions> = ArrayList<MarkerOptions>()
+        var minLat = 53.0
+        var maxLat = 53.5
+        var minLng = 9.0
+        var maxLng = 10.0
+        courts.forEach { court ->
+            if (court.latitude.isNotEmpty() && court.longitude.isNotEmpty()) {
+                val marker = MarkerOptions().position(LatLng(court.latitude.toDouble(), court.longitude.toDouble()))
+                marker.title(court.toString())
+                markers.add(marker)
             }
         }
+        markers.forEach {
+                marker ->
+            if (minLat > marker.position.latitude) { minLat = marker.position.latitude }
+            if (maxLat < marker.position.latitude) { maxLat = marker.position.latitude }
+            if (minLng > marker.position.longitude) { minLng = marker.position.longitude }
+            if (maxLng < marker.position.longitude) { maxLng = marker.position.longitude }
+            mMap.addMarker(marker)
+        }
+        val bounds = LatLngBounds(LatLng(minLat, minLng), LatLng(maxLat, maxLng))
+        val update: CameraUpdate = CameraUpdateFactory.newLatLngBounds(bounds, padding)
+        mMap.moveCamera(update)
+        Log.d("LatLng", "$minLat $minLng")
     }
 }

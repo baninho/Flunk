e4645fc514ae1642f507a09a5e1029a5bf3c08fa
baninho <florianimbt@gmail.com>
2021-02-14 23:46:34 -0600

    Increment player count in firestore when player joins

--- app/src/main/java/dev/baninho/flunk/ui/JoinCourtDialog.kt
+++ app/src/main/java/dev/baninho/flunk/ui/JoinCourtDialog.kt
@@ -4,14 +4,23 @@ import android.app.AlertDialog
 import android.app.Dialog
 import android.content.DialogInterface
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import androidx.fragment.app.DialogFragment
+import androidx.lifecycle.ViewModelProvider
 import com.google.android.gms.maps.model.Marker
 import dev.baninho.flunk.R
 import dev.baninho.flunk.dto.Court
 import java.lang.IllegalStateException
 
 class JoinCourtDialog(private val marker: Marker) : DialogFragment() {
+
+    private lateinit var viewModel: MainViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        viewModel = ViewModelProvider.AndroidViewModelFactory.getInstance(this.requireActivity().application).create(MainViewModel::class.java)
+    }
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         return activity?.let {
@@ -28,19 +37,24 @@ class JoinCourtDialog(private val marker: Marker) : DialogFragment() {
     }
 
     private fun joinCourt() {
+        val court = marker.tag as Court
 
+        // TODO: remove toast
         Toast.makeText(activity, "Joined court ${marker.title}", Toast.LENGTH_LONG).show()
 
-        // TODO: implement join court logic
+        // TODO: GPS check
+        Log.d("Players", "Player count: ${court.players}/${court.capacity}")
+        court.players += 1
+        viewModel.saveCourt(court)
 
-        (marker.tag as Court).players += 1
+        // TODO: fix inconsistency
+
     }
 
     private fun cancelJoinCourt() {
 
         Toast.makeText(activity, "Cancelled join court", Toast.LENGTH_LONG).show()
 
-        // TODO: implement cancel join court logic
     }
 
 }

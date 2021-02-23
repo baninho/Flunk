53b06531033baf32bcdc21ce91b853de9d95dad9
baninho <florianimbt@gmail.com>
2021-02-23 07:24:26 -0600

    Remove MainViewModel reference

--- app/src/main/java/dev/baninho/flunk/ui/MainActivity.kt
+++ app/src/main/java/dev/baninho/flunk/ui/MainActivity.kt
@@ -4,13 +4,18 @@ import android.os.Build
 import android.os.Bundle
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
 import dev.baninho.flunk.R
 
 class MainActivity : AppCompatActivity() {
 
+
+    private lateinit var mainViewModel: MainViewModel
+
     @RequiresApi(Build.VERSION_CODES.M)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        mainViewModel = ViewModelProvider(this).get(MainViewModel::class.java)
         setContentView(R.layout.activity_main)
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()

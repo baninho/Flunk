c964378a6ba676219baca45f2523f192fc3bc693
baninho <florianimbt@gmail.com>
2021-02-23 07:23:53 -0600

    Remove infinite court.join loop

--- app/src/main/java/dev/baninho/flunk/ui/JoinCourtDialog.kt
+++ app/src/main/java/dev/baninho/flunk/ui/JoinCourtDialog.kt
@@ -7,11 +7,10 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
 import androidx.fragment.app.DialogFragment
-import androidx.lifecycle.ViewModelProviders
+import androidx.lifecycle.ViewModelProvider
 import com.google.android.gms.maps.model.Marker
 import dev.baninho.flunk.R
 import dev.baninho.flunk.dto.Court
-import java.lang.IllegalStateException
 
 class JoinCourtDialog(private val marker: Marker) : DialogFragment() {
 
@@ -19,7 +18,7 @@ class JoinCourtDialog(private val marker: Marker) : DialogFragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        mainViewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
+        mainViewModel = ViewModelProvider(requireActivity()).get(MainViewModel::class.java)
     }
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
@@ -56,10 +55,9 @@ class JoinCourtDialog(private val marker: Marker) : DialogFragment() {
                 Toast.makeText(activity, resources.getText(R.string.msgAlreadyJoined), Toast.LENGTH_LONG).show()
             }
             Court.CourtJoinCode.JOIN_REQUEST_ACCEPTED -> {
-                court.join(mainViewModel.user)
+                mainViewModel.saveCourt(court)
             }
         }
-        mainViewModel.saveCourt(court)
 
     }
 

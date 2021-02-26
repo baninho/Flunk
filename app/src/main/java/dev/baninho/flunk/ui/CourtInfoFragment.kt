447dd1ace93b1688f8fa624d1584a92113e78974
baninho <florianimbt@gmail.com>
2021-02-26 17:42:23 -0600

    Initial commit

---
+++ app/src/main/java/dev/baninho/flunk/ui/CourtInfoFragment.kt
@@ -0,0 +1,27 @@
+package dev.baninho.flunk.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import dev.baninho.flunk.R
+
+class CourtInfoFragment : Fragment() {
+
+    private lateinit var mainViewModel: MainViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        mainViewModel = ViewModelProvider(requireActivity()).get(MainViewModel::class.java)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_court_info, container, false)
+    }
+}
